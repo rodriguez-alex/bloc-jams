1bcedae1869ae89b1c057199511cf46f177d046a
Alex Rodriguez <rodriguez-alex@live.com>
2014-10-13 08:51:32 +0000

    added functionality to player controls

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -214,6 +214,19 @@ var createSongRow = function(songNumber, songName, songLength) {
  
  };
 
+  var updateSeekPercentage = function($seekBar, event) {
+    var barWidth = $seekBar.width();
+    var offsetX = event.pageX - $seekBar.offset().left;
+
+    var offsetXPercent = (offsetX  / barWidth) * 100;
+    offsetXPercent = Math.max(0, offsetXPercent);
+    offsetXPercent = Math.min(100, offsetXPercent);
+
+    var percentageString = offsetXPercent + '%';
+    $seekBar.find('.fill').width(percentageString);
+    $seekBar.find('.thumb').css({left: percentageString});
+  }
+
 
 // This 'if' condition is used to prevent the jQuery modifications
 // from happening on non-Album view pages.
@@ -223,12 +236,40 @@ if (document.URL.match(/\/album.html/)) {
    $(document).ready(function() {
     changeAlbumView(albumMarconi, 'album-picasso');
     changeAlbumView(albumPicasso, 'album-marconi');
+    setupSeekBars();
 
     $('.album-header-container img').on('click', function(){
       $('.album-container').toggleClass('active');
     });
    }); //end document ready
  }
+
+  var setupSeekBars = function() {
+ 
+   $seekBars = $('.player-bar .seek-bar');
+   $seekBars.click(function(event) {
+     updateSeekPercentage($(this), event);
+   });
+
+   $seekBars.find('.thumb').mousedown(function(event){
+      var $seekBar = $(this).parent();
+
+      $seekBar.addClass('no-animate');
+   
+      $(document).bind('mousemove.thumb', function(event){
+        updateSeekPercentage($seekBar, event);
+      });
+   
+      //cleanup
+      $(document).bind('mouseup.thumb', function(){
+        $seekBar.removeClass('no-animate');
+        $(document).unbind('mousemove.thumb');
+        $(document).unbind('mouseup.thumb');
+      });
+   
+    }); //mousedown
+ 
+ };
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
